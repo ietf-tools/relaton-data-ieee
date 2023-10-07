b78da1d296b205e94f19a1db8eea78bb87606573
stefanomunarini <ssteee@gmail.com>
2023-10-07 21:57:18 +0100

    chore: use 'secrets'

--- crawler.rb
+++ crawler.rb
@@ -5,8 +5,8 @@ require "fileutils"
 t1 = Time.now
 puts "Started at: #{t1}"
 
-puts ARGV.length
-token = ARGV[0]
+puts ENV.length
+token = ENV[0]
 system("git clone https://oauth2:#{token}@github.com/ietf-ribose/ieee-rawbib.git ieee-rawbib")
 require "relaton_ieee"
 FileUtils.rm_rf("data")
