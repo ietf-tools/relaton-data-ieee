14793626ad0b5599ead56df9efa3db72105f2a46
stefanomunarini <ssteee@gmail.com>
2023-10-07 15:31:54 +0100

    chore: update crawler

--- crawler.rb
+++ crawler.rb
@@ -5,15 +5,23 @@ require "fileutils"
 t1 = Time.now
 puts "Started at: #{t1}"
 
-system("git clone https://oauth2:#{${{ secrets.IETF_BIB_BOT_PAT }}}@github.com/ietf-tools/relaton-data-rawbib.git")
-require "relaton_ieee"
+# system("git clone https://oauth2:#{${{ secrets.IETF_BIB_BOT_PAT }}}@github.com/ietf-tools/relaton-data-rawbib.git")
+# require "relaton_ieee"
+
+token = ENV['secrets.IETF_BIB_BOT_PAT']
+puts token
+
+to = ${{ secrets.IETF_BIB_BOT_PAT }}
+puts to
+
+system("git clone https://#{token}@github.com/ietf-ribose/ieee-rawbib.git ieee-rawbib")
+
+FileUtils.rm_rf("data")
 RelatonIeee::DataFetcher.fetch
 
-system("git clone https://github.com/ietf-tools/relaton-data-ieee")
-FileUtils.rm_rf("data")
 FileUtils.cp_r("relaton-data-ieee/data", ".")
 FileUtils.rm_rf("relaton-data-ieee")
-FileUtils.rm_rf("relaton-data-rawbib")
+FileUtils.rm_rf("ieee-rawbib")
 
 t2 = Time.now
 puts "Stopped at: #{t2}"
